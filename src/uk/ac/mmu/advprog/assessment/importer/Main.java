b8b919b890e99c70b909aaca5f61b40d548d4554
josh.devey <josh.devey@jdplc.com>
2026-03-23 17:23:35 +0000

    moved jar to lib

--- src/uk/ac/mmu/advprog/assessment/importer/Main.java
+++ src/uk/ac/mmu/advprog/assessment/importer/Main.java
@@ -1,9 +1,15 @@
 package uk.ac.mmu.advprog.assessment.importer;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 	}
+	
 }

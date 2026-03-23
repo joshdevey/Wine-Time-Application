e849b7e64335091de08579398f86dcc91a1cdbac
josh.devey <josh.devey@jdplc.com>
2026-03-23 17:24:03 +0000

    removed imports

--- src/uk/ac/mmu/advprog/assessment/importer/Main.java
+++ src/uk/ac/mmu/advprog/assessment/importer/Main.java
@@ -1,10 +1,5 @@
 package uk.ac.mmu.advprog.assessment.importer;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 public class Main {
 
 	public static void main(String[] args) {
